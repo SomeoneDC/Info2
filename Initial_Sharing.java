857e323ecb007ffc2e93ed5cae4016c474fcd2d1
Tz3r0 <jannik-kocht@web.de>
2018-05-24 16:28:20 +0200

    Initial sharing of project

---
+++ Initial_Sharing.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author 
+ * @version 
+ */
+public class Initial_Sharing
+{
+    // Bezugsobjekte
+
+    // Attribute
+
+    // Konstruktor
+    public Initial_Sharing()
+    {
+        System.out.println("Test");
+    }
+
+    // Dienste
+
+}
